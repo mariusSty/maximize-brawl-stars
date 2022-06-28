a73713e030cea2b77531219ff44da5a956b39573
Marius STEPHANY <marius.stephany@gmail.com>
2022-06-28 19:25:06 +0200

    :fire: remove base app code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { getPlayer } from "./server";
 
 function App() {
@@ -9,21 +7,8 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-        <button onClick={testServer}></button>
-      </header>
+    <div>
+      <button onClick={testServer}></button>
     </div>
   );
 }
