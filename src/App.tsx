f0f2c9f200c4020504c46a0f1175dade9f23c051
Marius STEPHANY <marius.stephany@gmail.com>
2022-07-23 16:56:46 +0200

    :sparkles: add error and loading

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,38 @@ import { ResultInfos } from "./models";
 import { ResultWrapper } from "./ResultWrapper";
 import { SearchForm } from "./SearchForm";
 import { getAllBrawlers, getPlayer } from "./server";
+import { Spinner } from "./Spinner";
 import { compareBrawler } from "./utils";
 
 function App() {
   const [player, setPlayer] = useState(undefined);
   const [resultInfos, setResultInfos] = useState<ResultInfos | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorTagInput, setErrorTagInput] = useState<string | undefined>(undefined);
+
   const handleSubmit = async (tag: string) => {
+    setResultInfos(undefined);
+    setIsLoading(true);
     const playerInfos = await getPlayer(tag);
     const allBrawlers = await getAllBrawlers();
     const resultCompare = compareBrawler(allBrawlers.items, playerInfos.brawlers);
     setPlayer(playerInfos);
     setResultInfos(resultCompare);
+    setIsLoading(false);
   };
 
   return (
     <div className="flex flex-col font-mono bg-slate-300 h-screen">
       <Header></Header>
-      <SearchForm findPlayer={handleSubmit}></SearchForm>
-      <ResultWrapper player={player} resultInfos={resultInfos}></ResultWrapper>
+      <SearchForm findPlayer={handleSubmit} error={errorTagInput} setError={setErrorTagInput} setIsLoading={setIsLoading}></SearchForm>
+      {
+        isLoading &&
+        <Spinner />
+      }
+      {
+        player && resultInfos && !isLoading &&
+          <ResultWrapper player={player} resultInfos={resultInfos}></ResultWrapper>
+      }
     </div>
   );
 }
--- src/SearchForm.tsx
+++ src/SearchForm.tsx
@@ -1,26 +1,40 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, MouseEvent, useState } from "react";
 
 interface SearchFormProps {
   findPlayer: (tag: string) => Promise<void>
+  error?: string;
+  setError: (error: string | undefined) => void
+  setIsLoading: (isLoading: boolean) => void 
 }
 
-export const SearchForm: React.FC<SearchFormProps> = ({ findPlayer }) => {
+export const SearchForm: React.FC<SearchFormProps> = ({ findPlayer, error, setError,  setIsLoading }) => {
   const [tag, setTag] = useState("");
 
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    findPlayer(tag);
+  const handleSubmit = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setError(undefined);
+    findPlayer(tag).catch(() => { setError(`Impossible de trouver le joueur avec ce tag : "${tag}" !`); setIsLoading(false);});
   };
 
-  const onChange = (e: any) => {
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setError(undefined);
     setTag(e.target.value);
   };
 
   return (
-    <div className="flex justify-center py-10">
-      <div className="flex justify-center items-center h-full w-10 bg-slate-800 rounded-l-lg"><span className="text-gray-100 text-3xl">#</span></div>
-      <input placeholder= "Tag ?" className="h-10 px-3 text-lg placeholder-gray-600 border-2 border-slate-800 rounded-r-lg" type="text" name="tag" onChange={onChange}></input>
-      <button className="h-10 px-6 mx-3 text-lg text-gray-100 transition-colors duration-150 bg-slate-800 rounded-lg hover:bg-slate-700" onClick={handleSubmit}>Valider</button>
-    </div>
+    <>
+      <div className="flex justify-center py-10">
+        <div className="flex flex-col items-center max-w-screen-sm">
+          <div className="flex w-full">
+            <div className="flex justify-center items-center w-10 bg-slate-800 rounded-l-lg">
+              <span className="text-gray-100 text-3xl">#</span>
+            </div>
+            <input placeholder= "Tag ?" className="h-10 px-3 text-lg placeholder-gray-600 border-2 border-slate-800 rounded-r-lg" type="text" name="tag" onChange={onChange}></input>
+            <button className="h-10 px-6 mx-3 text-lg text-gray-100 transition-colors duration-150 bg-slate-800 rounded-lg hover:bg-slate-700" onClick={handleSubmit}>Valider</button>
+          </div>
+          <span className="absolute top-40 text-red-600 text-sm self-start">{error}</span>
+        </div>
+      </div>
+    </>
   );
 };
